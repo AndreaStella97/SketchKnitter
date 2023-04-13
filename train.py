71d3acf4cb695359d0bc52eefb7d0d1fd63845bc
Andrea <andrea.stella02@universitadipavia.it>
2023-04-13 15:32:23 +0200

    change category

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ def main():
         data_dir=args.data_dir,
         batch_size=args.batch_size,
         image_size=args.image_size,
-        category=["house.npz"],
+        category="house.npz",
         class_cond=False,
     )
 

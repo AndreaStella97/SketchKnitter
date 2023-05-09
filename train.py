2090ce216f004e31db4915a124b51cbca7e67022
Andrea <andrea.stella02@universitadipavia.it>
2023-05-09 22:05:35 +0200

    .

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ def main():
         data_dir=args.data_dir,
         batch_size=args.batch_size,
         image_size=args.image_size,
-        category=["house.npz"],
+        category=["airplane.npz"],
         class_cond=False,
     )
 
